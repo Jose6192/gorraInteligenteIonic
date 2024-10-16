34ca4a3ee3090bf81e2ff10254531d2c79eb6237
Jose6192 <127167425+Jose6192@users.noreply.github.com>
2024-10-16 16:13:56 -0500

    feat: :art: agrega pagina config

--- src/app/config/config.page.ts
+++ src/app/config/config.page.ts
@@ -1,20 +1,41 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonButton, IonIcon} from '@ionic/angular/standalone';
+import { RouterLink } from '@angular/router';
+import { addIcons } from 'ionicons';
+import { person } from 'ionicons/icons';
+import { TabsComponent } from "../tabs/tabs.component";
 
 @Component({
   selector: 'app-config',
   templateUrl: './config.page.html',
   styleUrls: ['./config.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [CommonModule, FormsModule, IonContent, IonButton, IonIcon, RouterLink, TabsComponent]
 })
 export class ConfigPage implements OnInit {
 
-  constructor() { }
+  constructor() {
+    addIcons({
+      'person-circle-outline': person
+    });
+  }
 
   ngOnInit() {
+  }
+
+  nombreUsuario: string = 'John Doe';
+  perfilUsuario: string = 'Admin';
+
+  agregarGorra() {
+    console.log('Agregar Gorra');
+    // Aquí puedes agregar la lógica para agregar gorras
+  }
+
+  logOut() {
+    console.log('Cerrar sesión');
+    // Aquí puedes agregar la lógica para cerrar sesión
   }
 
 }
